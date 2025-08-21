6a29db7c710d1abf0465dcbf1e27603b58a3c42d
Jjpablo28 <jpr.rod06@gmail.com>
2025-08-20 23:43:40 -0500

    Cambio cors

--- frontAngular/src/app/cesar-cipher/cesar-cipher.service.ts
+++ frontAngular/src/app/cesar-cipher/cesar-cipher.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CesarCipherService {
 
-  private apiUrlEncriptar = 'https://ciphercesar.onrender.com/api/cipher/encriptar'; 
-private apiUrlDesencriptar='https://ciphercesar.onrender.com/api/cipher/desencriptar';
+  private baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   encrypt(palabra: string, clave: number): Observable<any> {
-    return this.http.post(`${this.apiUrlEncriptar}`, { palabra, clave });
+    return this.http.post(`${this.baseUrl}/encriptar`, { palabra, clave });
   }
 
   decrypt(palabra: string, clave: number): Observable<any> {
-    return this.http.post(`${this.apiUrlDesencriptar}`, { palabra, clave });
+    return this.http.post(`${this.baseUrl}/desencriptar`, { palabra, clave });
   }
 }
---
+++ frontAngular/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ciphercesar.onrender.com/api/cipher'
+};
